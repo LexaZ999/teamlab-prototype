5e6a0ae664f00ed886f0cc3aa825bdeebf6c9d31
Aleksey Zubkov <zubkovf@mail.ru>
2022-05-03 16:21:43 +0700

    Update Feedback component

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -3,9 +3,9 @@ import { Container } from 'react-bootstrap';
 import { AiFillQuestionCircle } from 'react-icons/ai';
 
 const Feedback = () => (
-  <Container className="pt-5 pb-3">
+  <Container className="pt-5 pb-3 ">
     <div className="bg-light">
-      <div className="d-flex align-items-center ms-4 py-4">
+      <div className="d-flex align-items-center ms-4 py-4 justify-content-center">
         <AiFillQuestionCircle className="me-3" size={24} color="#a0bd27" />
         <p className="m-0 p-0 fs-6">
           Что-то не работает на сайте, нашел баг?
