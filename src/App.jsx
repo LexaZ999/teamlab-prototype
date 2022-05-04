b6ea4d2bc62a3bee867047ae5a4891db059b01cb
Aleksey Zubkov <zubkovf@mail.ru>
2022-05-04 16:44:15 +0700

    Add Userbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, HashRouter } from 'react-router-dom';
 import Feedback from './components/Feedback.jsx';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
+import Userbar from './components/Userbar.jsx';
 import AccelerationPrograms from './pages/AccelerationPrograms.jsx';
 import Internships from './pages/Internships.jsx';
 import MyProfile from './pages/MyProfile.jsx';
@@ -12,6 +13,7 @@ import Testing from './pages/Testing.jsx';
 const App = () => (
   <HashRouter>
     <Header />
+    <Userbar />
     <Routes>
       <Route path="*" element={<NotFound />} />
       <Route path="my-profile" element={<MyProfile />} />
